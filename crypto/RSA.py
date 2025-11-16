d9b3020b40bde9d781a5c3c179bad30ac9802b00
Sam <145058794+SamAzr@users.noreply.github.com>
2025-11-16 22:00:37 +0300

    RSA that encrypt / decrypt string AND AES Key
    it can encrypt / decrypt type(string) and type(byte)

--- crypto/RSA.py
+++ crypto/RSA.py
@@ -1,8 +1,9 @@
 import math
-from pydoc import plain
-
 from Crypto.Util import number
 import random
+import ast
+import os
+
 
 #Generate Random Prime numbers
 def generate_prime (bits):
@@ -53,26 +54,62 @@ def create_keys(name):
     sk = (n,d)
     return name,pk,sk
 
+# RSA Encryption for string and AES key
+# return list of strings
 def rsa_encryption(msg,pub_key):
-    msg_encoded = [ord(c) for c in msg]
-    cipher = [sqm(c,pub_key[1],pub_key[0]) for c in msg_encoded]
-    return cipher
+    if type(msg) is str:
+        msg_encoded = [ord(c) for c in msg]
+        cipher = [sqm(c,pub_key[1],pub_key[0]) for c in msg_encoded]
+        return cipher
+    else:
+        msg_to_str = str(msg)
+        msg_encoded = [ord(c) for c in msg_to_str]
+        cipher = [sqm(c, pub_key[1], pub_key[0]) for c in msg_encoded]
+        return cipher
 
-def rsa_decrypt(cipher, private_key):
-    cipher_encoded = [sqm(c, private_key[1], private_key[0]) for c in cipher]
+# RSA Decryption for AES key
+def rsa_aes_decrypt(cipher, priv_key):
+    cipher_encoded = [sqm(c, priv_key[1], private_key[0]) for c in cipher]
+    decrypted_text = "".join (chr(ch) for ch in cipher_encoded)
+    decrypted_key = ast.literal_eval(decrypted_text)
+    return decrypted_key
+
+# RSA Decryption for String
+def rsa_str_decrypt(cipher, priv_key):
+    cipher_encoded = [sqm(c, priv_key[1], private_key[0]) for c in cipher]
     decrypted_text = "".join (chr(ch) for ch in cipher_encoded)
     return decrypted_text
 
 # for registration, the client will create variable in its dictionary that
 # will take 3 variable, the username , the public key and private key
-user_name,pub_key,priv_key = create_keys("sam")
+user_name, public_key, private_key = create_keys("sam")
 
-# for testing, will remove in the end
-msg1 = "Insert you msg here"
-print(msg1)
-cipherMsg = rsa_encryption(msg1,pub_key)
-print(cipherMsg)
+# creating an random AES key of size 32 bytes = AES-256
+def aes_key_creation():
+    aes_key = os.urandom(32)
+    return aes_key
 
-leplain = rsa_decrypt(cipherMsg,priv_key)
+#Testing AES key encryption / decryption and making sure the decryption function returns a type(Byte)
+data = aes_key_creation()
+print(data)
+encrypted_aes_key_rsa = rsa_encryption(data,public_key)
+print(encrypted_aes_key_rsa)
+decrypted_aes_key_rsa = rsa_aes_decrypt(encrypted_aes_key_rsa,private_key)
+print(decrypted_aes_key_rsa)
 
-print(leplain)
+#Testing String encryption / decryption and making sure the decryption function returns the original String
+
+data_string = "some random words"
+print(data_string)
+encrypted_string_rsa = rsa_encryption(data_string,public_key)
+print(encrypted_string_rsa)
+decrypted_string_rsa = rsa_str_decrypt(encrypted_string_rsa,private_key)
+print(decrypted_string_rsa)
+
+#msg1 = "Insert you msg here"
+#print(msg1)
+#cipherMsg = rsa_str_encryption(msg1,public_key)
+#print(cipherMsg)
+
+#leplain = rsa_str_decrypt(cipherMsg,private_key)
+#print(leplain)
